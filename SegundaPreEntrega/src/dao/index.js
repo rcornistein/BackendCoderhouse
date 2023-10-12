5e5c33b02e04054c5181ea68f22dd33ff5493d2e
rcornistein <rominacornistein@gmail.com>
2023-10-12 12:21:48 -0300

    Delete SegundaPreEntrega directory

--- SegundaPreEntrega/src/dao/index.js
+++
@@ -1,9 +0,0 @@
-import { ProductManagerMongo } from "./mongo/productManagerMongo.js";
-import { CartManagerMongo } from "./mongo/cartManagerMongo.js";
-import { ChatManagerMongo } from "./mongo/chatManagerMongo.js";
-import { __dirname } from "../utils.js";
-import path from "path";
-
-export const productsService = new ProductManagerMongo();
-export const chatService= new ChatManagerMongo();
-export const cartsService = new CartManagerMongo();
